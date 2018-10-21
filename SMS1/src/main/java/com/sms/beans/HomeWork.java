150db676c01eb19688c5c3a986bce7ea813aabb0
ishwardeep <mail2id@yahoo.com>
2018-10-22 01:18:20 +0530

    added getter/setter

--- SMS1/src/main/java/com/sms/beans/HomeWork.java
+++ SMS1/src/main/java/com/sms/beans/HomeWork.java
@@ -42,6 +42,56 @@ public class HomeWork {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "teacher_id")
 	private Teacher teacher;
-	
-	
+
+	public int getHomeWorkId() {
+		return homeWorkId;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Section getSection() {
+		return section;
+	}
+
+	public void setSection(Section section) {
+		this.section = section;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
 }
