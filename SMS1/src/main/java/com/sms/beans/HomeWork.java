1598f38e44a15e8939d143aae0ef5495c416e9b0
ishwardeep <mail2id@yahoo.com>
2018-10-25 23:15:41 +0530

    remove redundant imports

--- SMS1/src/main/java/com/sms/beans/HomeWork.java
+++ SMS1/src/main/java/com/sms/beans/HomeWork.java
@@ -1,8 +1,5 @@
 package com.sms.beans;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -11,8 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Proxy;
